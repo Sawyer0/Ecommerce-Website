309187423a670e0efbc9e7cb34924fcc2b11378b
Dawan Sawyer <dawansawyer@gmail.com>
2022-01-08 14:48:14 -0500

    created featured movie component with picture and play and info button, working on div closing tag bug

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from '../components/featured/Featured'
 import Navbar from '../components/navbar/Navbar'
 import './home.scss'
 
@@ -5,13 +6,7 @@ export default function Home() {
     return (
         <div className = "home">
             <Navbar />
-            <img
-            width= "100%"
-            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            
-          />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur non ad, delectus minus totam libero facere repellendus doloremque incidunt cum corporis id nobis hic eius placeat enim, quasi sint. Quia!
+            <Featured type="movie"/>
         </div>
     )
 }
